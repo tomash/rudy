72574a52fa23e75231cc37acfec82f49883a9d25
Tomasz Stachewicz <tomasz.stachewicz@aenima.pl>
2009-02-22 21:08:16 +0100

    _bcd and _rubyd versions of dexter.d with appropiate compile.XXX scripts. cleaned up bcd.Ruby.ruby.d file so that compiler doesn't complain

--- dexter/dexter_bcd.d
+++ dexter/dexter_bcd.d
@@ -12,7 +12,7 @@ extern (C) VALUE Dexter = 0;
 // The initialization method for this module
 extern (C) void Init_dexter() {
   Dexter = rb_define_class("Dexter", rb_cObject);
-  //rb_define_method(Dexter, "return_ten".ptr, &method_return_ten, 0);
+  rb_define_method(Dexter, "return_ten".ptr, &method_return_ten, 0);
 }
 
 // Our 'test1' method.. it simply returns a value of '10' for now.
