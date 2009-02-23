cac6923e692a9b94f764f73900b820453ea25691
Tomasz Stachewicz <tomasz.stachewicz@aenima.pl>
2009-02-23 09:25:39 +0100

    rb_define_module and rb_define_module_function working

--- dexter/dexter_bcd.d
+++ dexter/dexter_bcd.d
@@ -6,13 +6,17 @@ import bcd.Ruby.ruby;
 alias VALUE(*func_type)();
 // Prototype for our method 'test1' - methods are prefixed by 'method_' here
 extern (C) VALUE method_return_ten();
-extern (C) VALUE Dexter = 0;
+extern (C) VALUE DexterClass = 0;
+extern (C) VALUE DexterModule = 0;
 
 
 // The initialization method for this module
 extern (C) void Init_dexter() {
-  Dexter = rb_define_class("Dexter", rb_cObject);
-  rb_define_method(Dexter, "return_ten".ptr, &method_return_ten, 0);
+  DexterClass = rb_define_class("DexterClass", rb_cObject);
+  rb_define_method(DexterClass, "return_ten".ptr, &method_return_ten, 0);
+  
+  DexterModule = rb_define_module("DexterModule");
+  rb_define_module_function(DexterModule, "return_ten".ptr, &method_return_ten, 0);
 }
 
 // Our 'test1' method.. it simply returns a value of '10' for now.
--- dexter/dexter_tester.rb
+++ dexter/dexter_tester.rb
@@ -5,9 +5,12 @@ require 'test/unit'
 
 class DexterTester < Test::Unit::TestCase
   def test_module_defined_properly
-    assert_equal(Dexter.class, Class)
-    assert_equal(Dexter.superclass, Object)
-    assert_equal(Dexter.new.return_ten, 10)
+    assert_equal(DexterClass.class, Class)
+    assert_equal(DexterClass.superclass, Object)
+    assert_equal(DexterClass.new.return_ten, 10)
+    
+    assert_equal(DexterModule.class, Module)
+    assert_equal(DexterModule::return_ten, 10)
   end
   
 end
