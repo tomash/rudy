55b114a5bf1a2f6515655b0bdcd8dfdb598da7db
Tomasz Stachewicz <tomasz.stachewicz@aenima.pl>
2009-05-04 08:58:51 +0200

    removed old & obsolete bindings from rubyd

--- ruby/c/defines.d
+++
@@ -1,8 +0,0 @@
-module ruby.c.defines;
-
-extern (C) {
-void *ruby_xmalloc ();
-void *ruby_xcalloc ();
-void *ruby_xrealloc ();
-void ruby_xfree ();
-}
--- ruby/c/dln.d
+++
@@ -1,7 +0,0 @@
-module ruby.c.dln;
-
-extern (C) {
-char *dln_find_exe ();
-char *dln_find_file ();
-void *dln_load ();
-}
