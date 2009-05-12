45550e65816c29a06778c23fdd0f8000a8a78168
Tomasz Stachewicz <tomasz.stachewicz@aenima.pl>
2009-05-12 23:56:38 +0200

    automated build: make it work with dsss AND tango (and dmd) on linux

--- rudy/lib_abstract.d
+++ rudy/lib_abstract.d
@@ -4,16 +4,18 @@ module rudy.lib_abstract;
 
 version (Rudy_with_Tango) {
     import tango.stdc.string : strlen;
-    import tango.text.convert.Integer : qadut;
+    //import tango.text.convert.Integer : qadut;
     char[] toString(char* s) {
         return s[0 .. strlen(s)];
     }
-    char[] toString(uint i) {
+    /*char[] toString(uint i) {
         char[64] tmp;
         return qadut(tmp, i);
-    }
+    }*/
 
-    public import tango.util.meta.Nameof : symbolnameof, prettytypeof, prettynameof;
+    //public import tango.util.meta.Nameof : symbolnameof, prettytypeof, prettynameof;
+    //
+    public import meta.Nameof : symbolnameof, prettytypeof, prettynameof;
     public import meta.Default : minArgs, ParameterTypeTuple, ReturnType;
     char[] objToStr(Object o) {
         return o.toUtf8();
